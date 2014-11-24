1c78e038ba6d75a2278670cd51d1711e15374752
John Foley <johnfoley3@gmail.com>
2014-11-23 18:52:45 -0600

    initial commit

---
+++ FoleyCMT.java
@@ -0,0 +1,17 @@
+// import statements
+
+/**
+ *  @author John Foley <jpf7324@truman.edu>
+ *  @version 0.1
+ *  @since 11.23.14
+ *
+ *  A solution to the common meeting time problem described in P1, written
+ *  in Java using jthreads.
+ */
+public class FoleyCMT {
+
+	public static void main(string[] args) {
+
+		System.out.print("Hello!");
+	}
+}
